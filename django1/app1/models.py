49147730990364026b2e7f6448ae5b1db39c7b08
rickrribeiro <rickribeiro99@gmail.com>
2021-02-22 16:49:40 -0300

    models, migrations and some annotations

--- django1/app1/models.py
+++ django1/app1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    preco = models.DecimalField('Preco', decimal_places=2, max_digits=8)
+    estoque = models.IntegerField('Qt estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)

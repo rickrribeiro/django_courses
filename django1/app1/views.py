928a9c6e7f12b793170cce18a45eb133bebf1d0b
rickrribeiro <rickribeiro99@gmail.com>
2021-02-22 18:54:42 -0300

    db itens to template

--- django1/app1/views.py
+++ django1/app1/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
-
+from .models import Produto
 # Create your views here.
 
 def index(request):
+    print(dir(request.user))
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuario logado'
+
+    produtos = Produto.objects.all()
     context ={
         'curso':'Programação django',
-        'outro':'outro param'
+        'outro':'outro param',
+        'logado' : teste,
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, id):
+    prod = Produto.objects.get(id=id)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)

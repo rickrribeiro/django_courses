cd223ce952bde999d5cd5d5e041bbc9593697d11
rickrribeiro <rickribeiro99@gmail.com>
2021-02-22 21:56:09 -0300

    confs

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g+7j(t=49zal6wxzdlhpd=)htjs=01ky2m!10mtvc)m1)wsfvl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] #colocar url quando for definida
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

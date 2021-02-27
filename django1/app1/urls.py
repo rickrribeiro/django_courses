04a7a9ace2772fd6062bbe22a972ae91f5a2e17d
rickrribeiro <rickribeiro99@gmail.com>
2021-02-27 19:02:36 -0300

    projeto finalizado

--- django1/app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index,contato, produto
-
-
-urlpatterns=[
-    path('',index, name='index'),
-    path('contato',contato),
-    path('produto/<int:id>', produto, name='produto') #não pode ter esaço depois do <int: para n dar erro
-]

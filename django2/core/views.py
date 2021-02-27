1b0b88f80e3c2d1e2c172d9eb8d4b87a88b13179
rickrribeiro <rickribeiro99@gmail.com>
2021-02-27 16:47:15 -0300

    user section

--- django2/core/views.py
+++ django2/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ContatoForm, ProdutoModelForm
 from django.contrib import messages
 from .models import Produto
+from django.shortcuts import redirect
 # Create your views here.
 
 def index(request):
@@ -26,17 +27,21 @@ def contato(request):
     return render(request, 'contato.html', context)
 
 def produto(request):
-    if str(request.method) == 'POST':
-        form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Produto salvo com sucesso.')
-            form = ProdutoModelForm()
+    print({request.user})
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto')
         else:
-            messages.error(request, 'Erro ao salvar produto')
+            form = ProdutoModelForm()
+        context ={
+            'form': form
+        }
+        return render(request, 'produto.html', context)
     else:
-        form = ProdutoModelForm()
-    context ={
-        'form': form
-    }
-    return render(request, 'produto.html', context)
+        return redirect('index')

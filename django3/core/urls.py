7db80fb6b12e8d19b750cba249330392ac1e516a
rickrribeiro <rickribeiro99@gmail.com>
2021-03-01 16:26:25 -0300

    error page

--- django3/core/urls.py
+++ django3/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import IndexView, TesteView
+from .views import IndexView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('teste', TesteView.as_view(), name='teste'),
+
 ]
--- django3/core/views.py
+++ django3/core/views.py
@@ -5,5 +5,3 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class TesteView(TemplateView):
-    template_name = 'teste.html'

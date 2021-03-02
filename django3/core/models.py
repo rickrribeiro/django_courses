e3cb4dcaa6d3513d97f2a7804c7cbffa72ebc4c6
rickrribeiro <rickribeiro99@gmail.com>
2021-03-02 13:09:21 -0300

    base model

--- django3/core/models.py
+++ django3/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from stdimage.models import StdImageField
 # Create your models here.
+
+class Base(models.Model):
+    criado = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract= True
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )

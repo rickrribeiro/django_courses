5aa4dd72c58c6fb118088c5529d8fc5524860997
rickrribeiro <rickribeiro99@gmail.com>
2021-03-05 10:24:03 -0300

    templates with websocket

--- django6/realtime/settings.py
+++ django6/realtime/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +136,4 @@ CHANNEL_LAYERS = {
             'hosts': [('127.0.0.1', 6379)]
         },
     },
+}

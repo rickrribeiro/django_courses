8b45f7993a499ed177b0416d20d30a706804cea7
rickrribeiro2 <ricardo.ramos@mindsight.com.br>
2024-06-04 03:22:46 -0300

    base chat course

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 - complete_django_course_2021(done in 2021): https://www.udemy.com/course/programacao-web-com-django-framework-do-basico-ao-avancado/?couponCode=IND21PM (web development with python and django)
 
 - advanced_django_rest: https://www.udemy.com/course/django-python-advanced (Build a Backend REST API with Python & Django - Advanced)
+
+
+https://learnxinyminutes.com/docs/python/
--- advanced_django_rest/app/app/settings.py
+++ advanced_django_rest/app/app/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('debug', 0)))
+#DEBUG = bool(int(os.environ.get('debug', 0)))
+DEBUG = os.environ.get('debug', 'False').lower() == 'true'
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(
         None,
